1bb7bc78f96f5657730d4e0f5d03a75ecdfea748
psharma501 <erpradeep.sharma227@gmail.com>
2023-08-21 09:50:13 +0530

    display data

--- Dom.js
+++ Dom.js
@@ -1,8 +1,34 @@
-function test(){
-  var uname = document.getElementById("name").value;
-  var uemail = document.getElementById("email").value;
-  
+let form = document.querySelector("form");
+let main = document.querySelector(".main");
+form.addEventListener("submit",(event)=>{
+  let name = event.target.name.value;
+  let email = event.target.email.value;
 
-  var stname = localStorage.setItem("uname",uname);
-  var stemail = localStorage.setItem("uemail",uemail);
+  var userData = JSON.parse(localStorage.getItem("userDetails")) ?? [];
+  userData.push({
+    'name':name,
+    'email':email
+  })
+
+  localStorage.setItem("userDetails",JSON.stringify(userData))
+  displayData();
+
+  event.preventDefault();
+})
+
+let displayData=() =>{
+  var userData = JSON.parse(localStorage.getItem("userDetails")) ?? [];
+  let finalData = '';
+  userData.forEach((element,i) => {
+    finalData+=`<div class = "items">
+    <span>items</span>
+    <h5>Name</h5>
+    <div>${element.name}</div>
+
+    <h5>Email</h5>
+    <div>${element.email}</div>
+  </div>`
+  });
+
+  main.innerHTML = finalData;
 }
